01e126ce6f5e83dca156285aaf9463c91a618938
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2018-09-30 19:12:07 +0100

    Correct spelling of "cipher"

--- R/encypher.R
+++ R/encypher.R
@@ -1,13 +1,13 @@
-#' Encypher
+#' Encipher
 #'
 #' @param plaintext A character vector of length 1 containing the text to be
-#'   encyphered
+#'   enciphered
 #' @param key A character vector of length 1 containing the key to be used to
-#'  encypher the plaintext
-#' @return A character vector of length 1 containing the encyphered text
+#'  encipher the plaintext
+#' @return A character vector of length 1 containing the enciphered text
 #' @export
 
-encypher <- function(plaintext, key) {
+encipher <- function(plaintext, key) {
   plaintext <- toupper(gsub(" ", "", plaintext))
   plaintext <- strsplit(plaintext, "")[[1]]
   key <- toupper(gsub(" ", "", key))
@@ -16,6 +16,6 @@ encypher <- function(plaintext, key) {
   key <- key[1:length(plaintext)]
   num_plain <- sapply(plaintext, match, LETTERS)
   num_key <- sapply(key, match, LETTERS)
-  num_cypher <- mapply(cypher::modulo_add, num_plain, num_key)
-  return(paste(LETTERS[num_cypher], collapse = ""))
+  num_cipher <- mapply(cipher::modulo_add, num_plain, num_key)
+  return(paste(LETTERS[num_cipher], collapse = ""))
 }
